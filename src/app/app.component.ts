6e72f4068a358f1a3c354df889bccdaf444300d5
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-07-02 12:24:11 +0500

    subscribed to the movies service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,13 @@ export class AppComponent {
   MoviesCatagories!:[];
   constructor(private Db: MoviesDbService) { }
 
+  getData(){
+    // this.MoviesCatagories
+    this.Db.getMoviesCatagories().subscribe({
+      next: (v) => this.MoviesCatagories = v.results,
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
+    });
+    this.Db.getMoviesCatagories().subscribe()
+  }
 }
