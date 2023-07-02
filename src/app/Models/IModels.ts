b78ea84134f26a161882b7801d7f0ae133b3176e
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-07-02 12:25:18 +0500

    Movies Db initial interface Representation

---
+++ src/app/Models/IModels.ts
@@ -0,0 +1,55 @@
+export interface MoviesTitlesPage {
+    page: number
+    next: string
+    entries: number
+    results: MoveTitleDetails[]
+  }
+export interface MoveTitleDetails {
+    _id: string
+    id: string
+    primaryImage: PrimaryImage
+    titleType: TitleType
+    titleText: TitleText
+    originalTitleText: OriginalTitleText
+    releaseYear: ReleaseYear
+    releaseDate: Date
+  }
+  
+  export interface PrimaryImage {
+    id: string
+    width: number
+    height: number
+    url: string
+    caption: Caption
+    __typename: string
+  }
+  
+  export interface Caption {
+    plainText: string
+    __typename: string
+  }
+  
+  export interface TitleType {
+    text: string
+    id: string
+    isSeries: boolean
+    isEpisode: boolean
+    __typename: string
+  }
+  
+  export interface TitleText {
+    text: string
+    __typename: string
+  }
+  
+  export interface OriginalTitleText {
+    text: string
+    __typename: string
+  }
+  
+  export interface ReleaseYear {
+    year: number
+    endYear: any
+    __typename: string
+  }
+  
