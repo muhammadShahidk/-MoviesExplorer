831ac1a8858d7badc83312d5ae53fa0c256d71aa
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-07-04 10:18:01 +0500

    detecting the input change in list

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MoviesDbService } from '../Services/movies-db.service';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { MoviesDbService, TitlesOPtions } from '../Services/movies-db.service';
 import { MoveTitleDetails, MoviesTitlesPage } from '../Models/IModels';
 
 @Component({
@@ -9,18 +9,49 @@ import { MoveTitleDetails, MoviesTitlesPage } from '../Models/IModels';
 })
 export class MoviesListComponent {
   constructor(private Moves:MoviesDbService) { }
+  @Input() genre!:string;
   // Page!:;
   MoviesTitles!:MoveTitleDetails[];
 
-  ngOnInit(): void {
-    // this.Moves.getMoviesTitles({genre:'Action',page:'1',year:2022,list:'most_pop_movies',limit:10}).subscribe( {
-    //   next: (v:MoviesTitlesPage) => this.MoviesTitles = v.results.filter((x)=>x.primaryImage != null),
-    //   error: (e) => console.error(e),
-    //   complete: () => console.log(this.MoviesTitles)
-    //   // console.log(this.MoviesTitles.Results.map((x)=>x.titleText.text));
-    // });
-
-    this.Moves.getLocalMoviesTitles().subscribe( r=>this.MoviesTitles = r.results.filter((x)=>x.primaryImage != null));
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['genre']) {
+     console.log(changes['genre'].currentValue);
+     this.Moves.getMoviesTitles({genre:this.genre,page:'1',year:2001,list:'most_pop_movies',limit:10}).subscribe( {
+      next: (v:MoviesTitlesPage) => this.MoviesTitles = v.results.filter((x)=>x.primaryImage != null),
+      error: (e) => console.error(e),
+      complete: () => console.log(this.MoviesTitles)
+      // console.log(this.MoviesTitles.Results.map((x)=>x.titleText.text));
+    });
+    }
   }
+
+  ngOnInit(): void {
+    this.Moves.getMoviesTitles({genre:'Action',page:'1',year:2001,list:'most_pop_movies',limit:10}).subscribe( {
+      next: (v:MoviesTitlesPage) => this.MoviesTitles = v.results.filter((x)=>x.primaryImage != null),
+      error: (e) => console.error(e),
+      complete: () => console.log(this.MoviesTitles)
+      // console.log(this.MoviesTitles.Results.map((x)=>x.titleText.text));
+    });  
+  }
+  
+  filteredOptions(newOptions:TitlesOPtions){
+    console.log(newOptions);
+    // debugger;
+    this.Moves.getMoviesTitles(newOptions).subscribe( {
+      next: (v:MoviesTitlesPage) => this.MoviesTitles = v.results.filter((x)=>x.primaryImage != null),
+      error: (e) => console.error(e),
+      complete: () => console.log(this.MoviesTitles)
+      // console.log(this.MoviesTitles.Results.map((x)=>x.titleText.text));
+    });
+  }
+     //this.Moves.getLocalMoviesTitles().subscribe( r=>this.MoviesTitles = r.results.filter((x)=>x.primaryImage != null));
+    //  const localMoviesTitles = localStorage.getItem('MoviesTitles');
+
+    //  const moviesTitles: MoviesTitlesPage = JSON.parse(localMoviesTitles?localMoviesTitles:'');
+    //   this.MoviesTitles = moviesTitles.results.filter((x)=>x.primaryImage != null);
+    //   console.log(this.MoviesTitles);
+    //   console.log("here is the local movies titles");
+
+  
 
 }
