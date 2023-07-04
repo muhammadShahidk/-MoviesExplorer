e5fbadc0cc901a914d3fbeec295d92c846dcbee2
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-07-04 10:20:15 +0500

    sidebar component

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+sibarOptions = [
+,"Action"
+,"Adventure"
+,"Animation"
+,"Biography"
+,"Comedy"
+,"Crime"
+,"Documentary"
+,"Drama"
+,"Family"
+,"Fantasy"
+,"Film-Noir"
+,"Game-Show"
+,"History"
+,"Horror"
+,"Music"
+,"Musical"
+,"Mystery"
+,"News"
+,"Reality-TV"
+,"Romance"
+,"Sci-Fi"
+,"Short"
+,"Sport"
+,"Talk-Show"
+,"Thriller"
+,"War"
+,"Western"
+
+]
+
+@Output() selectedOption = new EventEmitter<string>();
+HandleClick(event:MouseEvent){
+  const target = event.target as HTMLElement;
+  this.selectedOption.emit(target.textContent ?? '');
+}
+
+
 
 }
