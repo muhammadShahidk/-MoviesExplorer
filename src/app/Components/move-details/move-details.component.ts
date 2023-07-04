7b5975f87365e323759e72576e74368e4d33adce
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-07-04 11:40:27 +0500

    details component initial

---
+++ src/app/Components/move-details/move-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-move-details',
+  templateUrl: './move-details.component.html',
+  styleUrls: ['./move-details.component.css']
+})
+export class MoveDetailsComponent {
+
+}
